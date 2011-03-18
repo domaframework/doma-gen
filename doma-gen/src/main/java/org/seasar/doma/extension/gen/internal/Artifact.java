914544d168d368bde958fc396d87e0dce8027ba0
nakamura-to <toshihiro.nakamura@gmail.com>
2011-03-18 13:47:05 +0000

    [DOMA-227	][Core] ドメインクラスが他のドメインクラスを継承しアクセサメソッドを親クラスに持つ場合、バインド変数に関連付けることができない問題に対応しました。 1.4.0開発開始。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@979 362887e2-e868-44d1-a061-b03550a0b845

--- doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
+++ doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
@@ -23,7 +23,7 @@ public class Artifact {
 
     private static final String NAME = "Doma-Gen";
 
-    private static final String VERSION = "1.13.1-SNAPSHOT";
+    private static final String VERSION = "1.14.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
