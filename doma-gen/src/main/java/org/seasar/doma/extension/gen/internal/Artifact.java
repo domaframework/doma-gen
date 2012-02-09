b0b0c0a19c2f0cfdcb8962674311d517bea2392b
nakamura-to <toshihiro.nakamura@gmail.com>
2012-02-09 14:05:00 +0000

    [DOMA-255][Core] IterationCallback実行後に任意の処理を行えるようにしました。
    git-svn-id: https://www.seasar.org/svn/doma/trunk@1073 362887e2-e868-44d1-a061-b03550a0b845

--- doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
+++ doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
@@ -23,7 +23,7 @@ public class Artifact {
 
     private static final String NAME = "Doma-Gen";
 
-    private static final String VERSION = "1.20.2-SNAPSHOT";
+    private static final String VERSION = "1.21.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
