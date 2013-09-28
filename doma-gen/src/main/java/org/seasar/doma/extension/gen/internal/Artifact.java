ac11b0fa8ca34c0048e05c2520c4b5999d561b64
nakamura-to <toshihiro.nakamura@gmail.com>
2013-09-28 06:41:59 +0000

    1.36.0 開発開始
    git-svn-id: https://www.seasar.org/svn/doma/trunk@1285 362887e2-e868-44d1-a061-b03550a0b845

--- doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
+++ doma-gen/src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
@@ -23,7 +23,7 @@ public class Artifact {
 
     private static final String NAME = "Doma-Gen";
 
-    private static final String VERSION = "1.35.0";
+    private static final String VERSION = "1.36.0-SNAPSHOT";
 
     public static String getName() {
         return NAME;
