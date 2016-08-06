903e9725ae81115aee774b6e96c46f1e36a4a144
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2016-08-06 12:50:26 +0900

    Next development version [ci skip]

--- docs/sources/gen.rst
+++ docs/sources/gen.rst
@@ -251,7 +251,7 @@ Gradle_ で使用するための設定例を示します。
   }
 
   dependencies {
-      domaGenRuntime 'org.seasar.doma:doma-gen:2.12.1'
+      domaGenRuntime 'org.seasar.doma:doma-gen:2.12.2-SNAPSHOT'
       domaGenRuntime 'org.postgresql:postgresql:9.3-1100-jdbc41'
   }
 
--- src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
+++ src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
@@ -23,7 +23,7 @@ public class Artifact {
 
     private static final String NAME = "Doma-Gen";
 
-    private static final String VERSION = "2.12.1";
+    private static final String VERSION = "2.12.2-SNAPSHOT";
 
     public static String getName() {
         return NAME;
