42e7dc6c43c83e928875e1b76e4f7692391c99d7
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2018-01-08 02:12:23 +0900

    2.19.1 released

--- src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
+++ src/main/java/org/seasar/doma/extension/gen/internal/Artifact.java
@@ -23,7 +23,7 @@ public class Artifact {
 
     private static final String NAME = "Doma-Gen";
 
-    private static final String VERSION = "2.20.0-SNAPSHOT";
+    private static final String VERSION = "2.19.1";
 
     public static String getName() {
         return NAME;
